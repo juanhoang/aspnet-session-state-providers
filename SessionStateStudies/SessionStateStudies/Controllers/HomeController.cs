8e8abd2e8cb932a3ce03a875eb7cd8356096694e
juanonsoftware <huanhvhd@gmail.com>
2015-06-22 17:35:58 +0700

    V1 - use StateServer on the same server

---
+++ SessionStateStudies/SessionStateStudies/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace SessionStateStudies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            TrySetAndGetFromSession(ViewBag);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private void TrySetAndGetFromSession(dynamic viewBag)
+        {
+            if (Session["LastVisit"] == null)
+            {
+                Session["LastVisit"] = DateTime.Now;
+            }
+            viewBag.LastVisit = Session["LastVisit"];
+        }
+    }
+}
---
+++ SessionStateStudies/SessionStateStudies/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SessionStateStudies.MvcApplication" Language="C#" %>
